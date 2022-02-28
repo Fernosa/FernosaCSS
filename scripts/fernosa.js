50117a390a0be1cdd287b45d7b5f7929fb946012
adityeahh <69644334+Adityeahhh@users.noreply.github.com>
2022-02-28 22:10:42 +0530

    added the sidenav script

---
+++ scripts/fernosa.js
@@ -0,0 +1,26 @@
+const sidenavHam = document.querySelector('#sidenav-burger');
+const sidenav = document.querySelector('#fernosa-sidenav')
+const closeSideNav = document.querySelector('#close-sidenav')
+
+
+sidenavHam.addEventListener("click", () => {
+    if (sidenav.classList.contains('sidenav-expand')) {
+        sidenav.classList.remove('sidenav-expand')
+    } else {
+        sidenav.classList.add('sidenav-expand')
+    }
+})
+
+closeSideNav.addEventListener("click", () => {
+    if (sidenav.classList.contains('unhidden')) {
+        sidenav.classList.add('w0')
+        sidenav.classList.remove('unhidden')
+        closeSideNav.classList.add('fa-chevron-right')
+    } else {
+        sidenav.classList.remove('w0')
+        sidenav.classList.add('unhidden')
+        closeSideNav.classList.remove('fa-chevron-right')
+    }
+})
+
+
